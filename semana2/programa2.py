0a138e2b514ceb5163bc90c7d0d712372319e443
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-01-24 14:04:39 +0000

    Variable utilizando llaves

---
+++ semana2/programa2.py
@@ -0,0 +1,11 @@
+"""
+    programa1
+    Nombre: Juan Carlos Rosales 
+    Vargas
+    Fecha: 23/01/2023
+    Descripción: Variables utilizando llaves, se llaman forma
+"""
+variable1 = " Hola" #variable para almacenar una cadena de caracteres
+variable2 = "619" #variable para alamcenar una cadena de variables
+print(variable1 + variable2) #concatenar
+print("{}{}".format(variable1,variable2)) #variables utilizando llaves
