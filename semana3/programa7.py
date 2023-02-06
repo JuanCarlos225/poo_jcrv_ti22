0b26f3e62c6402d85b720b169963d1fa1b1cfbef
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-02-06 03:08:34 +0000

    ejercicios de sumas, restas, multiplicación

---
+++ semana3/programa7.py
@@ -0,0 +1,17 @@
+numero1 = input("Numero1:")
+numero2 = 10
+suma = int(numero1) + numero2
+print(suma)
+
+resta = int(numero1) - numero2
+print(resta)
+
+
+multiplicación = int(numero1) * numero2
+print(multiplicación)
+
+división = int(numero1) / numero2
+print(división)
+
+potencia = int(numero1) ** numero2
+print(potenicia)
