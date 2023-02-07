a901bf15edd1091f1a6eb253b6598499059f6698
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-02-07 05:10:21 +0000

    Repaso para hallar el área de un triangulo

---
+++ semana3/programa8.py
@@ -0,0 +1,7 @@
+altura = float (input ('Ingresa el valor de altura: '))
+base = float (input ('Ingresa el valor de base: '))
+area=base*altura/2
+perimetro = base+base+base
+print ('Valor de area: ' + repr (area))
+print ('Valor de perimetro: ' + repr (perimetro))
+print 
