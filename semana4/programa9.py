0ee4b086253e473c1891afd1ff5149480d7ee03b
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-02-07 14:53:31 +0000

    programa de 11 versiones de if

---
+++ semana4/programa9.py
@@ -0,0 +1,16 @@
+n1=    int(input("N1"))
+n2=    int(input("N2"))
+if n1>n2:
+    print(n1)
+    if n2>n2:
+    print(n2)
+    if n1==n2:
+        print(None)
+
+if n2>n1:
+    print(n2)
+if n1>n2:
+    print(n1)
+if n2==n1:
+    print(None)
+            
