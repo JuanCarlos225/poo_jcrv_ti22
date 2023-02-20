fdd0c3261b7f9038420d58322b183c05e57bf22e
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-02-20 21:09:21 +0000

    Colocación de cometarios

--- semana4/programa10.py
+++ semana4/programa10.py
@@ -1,21 +1,14 @@
-def mayor(numero1, numero2):
-    if numero1>numero2:
+def mayor(numero1, numero2): # se define el valor de las variables numero1 y numero2
+    if numero1>numero2: 
         print(numero1)
     elif numero2 > numero1:
-        print(numero2)
+        print(numero2) 
     else:
-        print("none")
+        print("none") #imprime si las variables son none
         
 mayor (3,2)# 3 call
+mayor (2,3) #3
+mayor (3,3) #none
 
-def  mayor(numero1:int,numero2:int): #se vide las variables 
-       mayor=None#
- if numero1 > numero2:# se hace la operación para saber que numero es mayor
-   mayor = (numero1) # se imprime el número 1 como mayor
- elif numero2>numero1:# en caso que no sea real se pasa al siguiente operación para saber si el numero dos es mayor
-  mayor=(numero2)# se imprime el resultado del numero mayor en este caso es numero 2
- else:  # en caso que no se cumpla la función de artiba se pasa hacia abajo 
-   mayor= None# se denomida el mayor como none es decir que puede ser cualquier valor
- return mayor# se vuelve hacer tdo lo anterior es decir de reinicia el proceso.
 
      
