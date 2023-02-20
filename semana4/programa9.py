4c050e4f4c11be9104883cbbed67b1dcaada803d
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-02-20 20:39:05 +0000

    Actualización de comentarios

--- semana4/programa9.py
+++ semana4/programa9.py
@@ -1,16 +1,13 @@
-n1=    int(input("N1"))
-n2=    int(input("N2"))
-if n1>n2:
-    print(n1)
-    if n2>n2:
-    print(n2)
-    if n1==n2:
-        print(None)
+def mayor(numero1,numero2): # se define el valor de las variables numero1 y numero2
 
-if n2>n1:
-    print(n2)
-if n1>n2:
-    print(n1)
-if n2==n1:
-    print(None)
-            
+    if numero1>numero2:# se hace la operacion para saber si numero1 es mayor que numero2
+      print("el numero1 es mayor :{}".format(numero1)) # se imprime el resultado de la variable numero1
+    elif numero2>numero1: # se hace la operacion para saber si numero2 es mayor que numero1
+      print("el numero2 es mayor :{}".format(numero2)) # se imprime el resultado de la variable numero2
+
+    else:
+     print("iguales") # imprime si las variables son iguales
+
+    mayor(3,2) # se imprime que numero es mayor
+    mayor(2,3) #se imprime que variable es menor
+    mayor(3,3)#se imprime los valores que son iguales
