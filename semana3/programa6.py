3d3dc7b880ca8c3f249d6a02e9fa7a7e19468414
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-02-20 20:30:29 +0000

    Actualización de comentarios

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -1,5 +1,10 @@
-# problema comparar 2 números enteros e imprimir el número mayor
-int(input('Ingresa un número'))
-int(input("Ingresa otro número"))
-numero1=1
-numero2=2 
+
+
+R=float(input("Escribe el radio del circulo :"))# se pide el valor de radio
+circunferencia=2*math.PI*R #formula para la circunferencia de un circulo
+area=math.PI*R*R# se hace la operacion para sacar el area del circulo
+superficiedearea=4*math.PI*R*R# se hace la operacion para sacar la superficie de area
+print ( "Circunferencia:% .2f" % circunferencia) # imprime  la circunferencia del circulo
+print ( "Area del circulo:% .2f"% area) # imprimer el resultado del area del circulo
+print ( "Area de superficie del circulo:% .2f"%superficiedearea)# se imprime el resultado de la superficie del circulo
+
