910d4cde11c17fbc688034912a0a8214d1e8f600
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2023-02-20 20:20:50 +0000

    Actualización de comentarios

--- README.md
+++ README.md
@@ -7,14 +7,14 @@
 |1|programa0.py|Hola mundo en Python3|
 |2|programa1.py|Comentarios multilinea, comentarios de una linea, concatenación de una o más cadenas de texto|
 |3|programa2.py| Variables utilizando llaves, se llaman forma|
-|3|programa3.py|sintaxis de str.format|
-|4|programa4.py|input|
-|5|ejercicio.py|calcular area y perimetro de los triangulo1
-|6|programa5.py|calcular area y perimetro del circulo y cuadrado|
-|7| programa6.py|pruebas unitarias|
-|8|practica1.pi|ejercicio de sumas|
-|9|ejercicio|ejercicio2|
-|10|programa8.py|repaso para hallar un área de un triangulo|
-|11|programa9.py|programa de 11 versiones de if|
-|12|programa10.py|repaso de if1|
-|13|programa12,py|repaso de if1|
+|4|programa3.py|sintaxis de str.format|
+|5|programa4.py|input|
+|6|ejercicio.py|calcular area y perimetro de los triangulo1
+|7|programa5.py|calcular area y perimetro del circulo y cuadrado|
+|8| programa6.py|pruebas unitarias|
+|9|practica1.pi|ejercicio de sumas|
+|10|ejercicio|ejercicio2|
+|11|programa8.py|repaso para hallar un área de un triangulo|
+|12|programa9.py|programa de 11 versiones de if|
+|13|programa10.py|repaso de if1|
+|14|programa12.py|repaso de if1|
--- semana3/practica4.py
+++ semana3/practica4.py
@@ -1,4 +1,12 @@
-numero1 = int(input("Ingrese el primer número: "))
-numero2 = int(input("Ingrese el segundo número: "))
-suma = numero1 + numero2
-print("La suma es: ", suma)
+numero1 = int(input(" Numero 1:  "))  #  iput devuelve siempre un tipo de dato string, con la funcion casting decidimos que tipo de dato se requiere
+numero2 = 10  #  Tipo de dato int  
+suma = int(numero1) + numero2  #  Tambien se puede hacer el casting cuando utilicemos el dato 
+resta = numero1-numero2# se hace la operacion de las dos variables que son numero1 y numero2
+multiplicacion = numero1*numero2# se hace la operacion de las dos variables que son numero1 y numero2
+division = numero1/numero2#Se hace la operacion de las dos variables que son numero1 y numero2
+potencia = numero1**numero2 # se hace la operacion de la dos variables que son numero1 y numero2
+print ("suma =",suma) # se imprime el resultado de la suma
+print ("resta =",resta) # se imprime el resultado de la resta
+print ("Multiplicación =",multiplicacion) # se imprime el resultado de la multiplicacion
+print ("División =",division) # se  imprime el resultado de la division
+print ("Potencia =",potencia) #se imprime el resultado de la potencia
